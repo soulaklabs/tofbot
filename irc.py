0e3e6a4c5c17fedcd6d5e1384f9be2d81c6f5cfd
Christophe-Marie Duquesne <chm.duquesne@gmail.com>
2015-08-30 20:59:05 +0200

    pep8ified irc.py

--- irc.py
+++ irc.py
@@ -7,200 +7,220 @@ Licensed under the Eiffel Forum License 2.
 http://inamidst.com/phenny/
 """
 
-import sys, re, time, traceback
-import socket, asyncore, asynchat
+import sys
+import re
+import time
+import traceback
+import socket
+import asyncore
+import asynchat
 
-class Origin(object): 
-   source = re.compile(r'([^!]*)!?([^@]*)@?(.*)')
 
-   def __init__(self, bot, source, args): 
-      match = Origin.source.match(source or '')
-      self.nick, self.user, self.host = match.groups()
+class Origin(object):
+    source = re.compile(r'([^!]*)!?([^@]*)@?(.*)')
 
-      if len(args) > 1: 
-         target = args[1]
-      else: target = None
+    def __init__(self, bot, source, args):
+        match = Origin.source.match(source or '')
+        self.nick, self.user, self.host = match.groups()
 
-      mappings = {bot.nick: self.nick, None: None}
-      self.sender = mappings.get(target, target)
+        if len(args) > 1:
+            target = args[1]
+        else:
+            target = None
 
-class Bot(asynchat.async_chat): 
-   def __init__(self, nick, name, channels, password=None): 
-      asynchat.async_chat.__init__(self)
-      self.set_terminator('\n')
-      self.buffer = ''
+        mappings = {bot.nick: self.nick, None: None}
+        self.sender = mappings.get(target, target)
 
-      self.nick = nick
-      self.user = nick
-      self.name = name
-      self.password = password
 
-      self.verbose = True
-      self.channels = channels or []
-      self.stack = []
+class Bot(asynchat.async_chat):
+    def __init__(self, nick, name, channels, password=None):
+        asynchat.async_chat.__init__(self)
+        self.set_terminator('\n')
+        self.buffer = ''
 
-      import threading
-      self.sending = threading.RLock()
+        self.nick = nick
+        self.user = nick
+        self.name = name
+        self.password = password
 
-   def initiate_send(self):
-      self.sending.acquire()
-      asynchat.async_chat.initiate_send(self)
-      self.sending.release()
+        self.verbose = True
+        self.channels = channels or []
+        self.stack = []
 
-   # def push(self, *args, **kargs): 
-   #    asynchat.async_chat.push(self, *args, **kargs)
+        import threading
+        self.sending = threading.RLock()
 
-   def __write(self, args, text=None): 
-      # print 'PUSH: %r %r %r' % (self, args, text)
-      try: 
-         if text is not None: 
-            # 510 because CR and LF count too, as nyuszika7h points out
-            self.push((' '.join(args) + ' :' + text)[:510] + '\r\n')
-         else: self.push(' '.join(args)[:510] + '\r\n')
-      except IndexError: 
-         pass
+    def initiate_send(self):
+        self.sending.acquire()
+        asynchat.async_chat.initiate_send(self)
+        self.sending.release()
+    # def push(self, *args, **kargs):
+    #     asynchat.async_chat.push(self, *args, **kargs)
 
-   def write(self, args, text=None): 
-      # This is a safe version of __write
-      def safe(input): 
-         input = input.replace('\n', '')
-         input = input.replace('\r', '')
-         return input.encode('utf-8')
-      try: 
-         args = [safe(arg) for arg in args]
-         if text is not None: 
-            text = safe(text)
-         self.__write(args, text)
-      except Exception, e: pass
+    def __write(self, args, text=None):
+        # print 'PUSH: %r %r %r' % (self, args, text)
+        try:
+            if text is not None:
+                # 510 because CR and LF count too, as nyuszika7h points out
+                self.push((' '.join(args) + ' :' + text)[:510] + '\r\n')
+            else:
+                self.push(' '.join(args)[:510] + '\r\n')
+        except IndexError:
+            pass
 
-   def run(self, host, port=6667): 
-      self.initiate_connect(host, port)
+    def write(self, args, text=None):
 
-   def initiate_connect(self, host, port): 
-      if self.verbose: 
-         message = 'Connecting to %s:%s...' % (host, port)
-         print >> sys.stderr, message,
-      self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-      self.connect((host, port))
-      try: asyncore.loop()
-      except KeyboardInterrupt: 
-         sys.exit()
+        # This is a safe version of __write
+        def safe(input):
+            input = input.replace('\n', '')
+            input = input.replace('\r', '')
+            return input.encode('utf-8')
+        try:
+            args = [safe(arg) for arg in args]
+            if text is not None:
+                text = safe(text)
+            self.__write(args, text)
+        except Exception, e:
+            pass
 
-   def handle_connect(self): 
-      if self.verbose: 
-         print >> sys.stderr, 'connected!'
-      if self.password: 
-         self.write(('PASS', self.password))
-      self.write(('NICK', self.nick))
-      self.write(('USER', self.user, '+iw', self.nick), self.name)
+    def run(self, host, port=6667):
+        self.initiate_connect(host, port)
 
-   def handle_close(self): 
-      self.close()
-      print >> sys.stderr, 'Closed!'
+    def initiate_connect(self, host, port):
+        if self.verbose:
+            message = 'Connecting to %s:%s...' % (host, port)
+            print >> sys.stderr, message,
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connect((host, port))
+        try:
+            asyncore.loop()
+        except KeyboardInterrupt:
+            sys.exit()
 
-   def collect_incoming_data(self, data): 
-      self.buffer += data
+    def handle_connect(self):
+        if self.verbose:
+            print >> sys.stderr, 'connected!'
+        if self.password:
+            self.write(('PASS', self.password))
+        self.write(('NICK', self.nick))
+        self.write(('USER', self.user, '+iw', self.nick), self.name)
 
-   def found_terminator(self): 
-      line = self.buffer
-      if line.endswith('\r'): 
-         line = line[:-1]
-      self.buffer = ''
+    def handle_close(self):
+        self.close()
+        print >> sys.stderr, 'Closed!'
 
-      # print 'GOT:', repr(line)
-      if line.startswith(':'): 
-         source, line = line[1:].split(' ', 1)
-      else: source = None
+    def collect_incoming_data(self, data):
+        self.buffer += data
 
-      if ' :' in line: 
-         argstr, text = line.split(' :', 1)
-      else: argstr, text = line, ''
-      args = argstr.split()
+    def found_terminator(self):
+        line = self.buffer
+        if line.endswith('\r'):
+            line = line[:-1]
+        self.buffer = ''
 
-      origin = Origin(self, source, args)
-      self.dispatch(origin, tuple([text] + args))
+        # print 'GOT:', repr(line)
+        if line.startswith(':'):
+            source, line = line[1:].split(' ', 1)
+        else:
+            source = None
 
-      if args[0] == 'PING': 
-         self.write(('PONG', text))
+        if ' :' in line:
+            argstr, text = line.split(' :', 1)
+        else:
+            argstr, text = line, ''
+        args = argstr.split()
 
-   def dispatch(self, origin, args): 
-      pass
+        origin = Origin(self, source, args)
+        self.dispatch(origin, tuple([text] + args))
 
-   def msg(self, recipient, text): 
-      self.sending.acquire()
+        if args[0] == 'PING':
+            self.write(('PONG', text))
 
-      # Cf. http://swhack.com/logs/2006-03-01#T19-43-25
-      if isinstance(text, unicode): 
-         try: text = text.encode('utf-8')
-         except UnicodeEncodeError, e: 
-            text = e.__class__ + ': ' + str(e)
-      if isinstance(recipient, unicode): 
-         try: recipient = recipient.encode('utf-8')
-         except UnicodeEncodeError, e: 
-            return
+    def dispatch(self, origin, args):
+        pass
 
-      # No messages within the last 3 seconds? Go ahead!
-      # Otherwise, wait so it's been at least 0.8 seconds + penalty
-      if self.stack: 
-         elapsed = time.time() - self.stack[-1][0]
-         if elapsed < 3: 
-            penalty = float(max(0, len(text) - 50)) / 70
-            wait = 0.8 + penalty
-            if elapsed < wait: 
-               time.sleep(wait - elapsed)
+    def msg(self, recipient, text):
+        self.sending.acquire()
 
-      # Loop detection
-      messages = [m[1] for m in self.stack[-8:]]
-      if messages.count(text) >= 5: 
-         text = '...'
-         if messages.count('...') >= 3: 
-            self.sending.release()
-            return
+        # Cf. http://swhack.com/logs/2006-03-01#T19-43-25
+        if isinstance(text, unicode):
+            try:
+                text = text.encode('utf-8')
+            except UnicodeEncodeError, e:
+                text = e.__class__ + ': ' + str(e)
+        if isinstance(recipient, unicode):
+            try:
+                recipient = recipient.encode('utf-8')
+            except UnicodeEncodeError, e:
+                return
 
-      def safe(input): 
-         input = input.replace('\n', '')
-         return input.replace('\r', '')
-      self.__write(('PRIVMSG', safe(recipient)), safe(text))
-      self.stack.append((time.time(), text))
-      self.stack = self.stack[-10:]
+        # No messages within the last 3 seconds? Go ahead!
+        # Otherwise, wait so it's been at least 0.8 seconds + penalty
+        if self.stack:
+            elapsed = time.time() - self.stack[-1][0]
+            if elapsed < 3:
+                penalty = float(max(0, len(text) - 50)) / 70
+                wait = 0.8 + penalty
+                if elapsed < wait:
+                    time.sleep(wait - elapsed)
 
-      self.sending.release()
+        # Loop detection
+        messages = [m[1] for m in self.stack[-8:]]
+        if messages.count(text) >= 5:
+            text = '...'
+            if messages.count('...') >= 3:
+                self.sending.release()
+                return
 
-   def notice(self, dest, text): 
-      self.write(('NOTICE', dest), text)
+        def safe(input):
+            input = input.replace('\n', '')
+            return input.replace('\r', '')
+        self.__write(('PRIVMSG', safe(recipient)), safe(text))
+        self.stack.append((time.time(), text))
+        self.stack = self.stack[-10:]
 
-   def error(self, origin): 
-      try: 
-         import traceback
-         trace = traceback.format_exc()
-         print trace
-         lines = list(reversed(trace.splitlines()))
+        self.sending.release()
 
-         report = [lines[0].strip()]
-         for line in lines: 
-            line = line.strip()
-            if line.startswith('File "/'): 
-               report.append(line[0].lower() + line[1:])
-               break
-         else: report.append('source unknown')
+    def notice(self, dest, text):
+        self.write(('NOTICE', dest), text)
 
-         self.msg(origin.sender, report[0] + ' (' + report[1] + ')')
-      except: self.msg(origin.sender, "Got an error.")
+    def error(self, origin):
+        try:
+            import traceback
+            trace = traceback.format_exc()
+            print trace
+            lines = list(reversed(trace.splitlines()))
 
-class TestBot(Bot): 
-   def f_ping(self, origin, match, args): 
-      delay = m.group(1)
-      if delay is not None: 
-         import time
-         time.sleep(int(delay))
-         self.msg(origin.sender, 'pong (%s)' % delay)
-      else: self.msg(origin.sender, 'pong')
-   f_ping.rule = r'^\.ping(?:[ \t]+(\d+))?$'
+            report = [lines[0].strip()]
+            for line in lines:
+                line = line.strip()
+                if line.startswith('File "/'):
+                    report.append(line[0].lower() + line[1:])
+                    break
+            else:
+                report.append('source unknown')
 
-def main(): 
-   # bot = TestBot('testbot', ['#d8uv.com'])
-   # bot.run('irc.freenode.net')
-   print __doc__
+            self.msg(origin.sender, report[0] + ' (' + report[1] + ')')
+        except:
+            self.msg(origin.sender, "Got an error.")
 
-if __name__=="__main__": 
-   main()
+
+class TestBot(Bot):
+    def f_ping(self, origin, match, args):
+        delay = m.group(1)
+        if delay is not None:
+            import time
+            time.sleep(int(delay))
+            self.msg(origin.sender, 'pong (%s)' % delay)
+        else:
+            self.msg(origin.sender, 'pong')
+    f_ping.rule = r'^\.ping(?:[ \t]+(\d+))?$'
+
+
+def main():
+    # bot = TestBot('testbot', ['#d8uv.com'])
+    # bot.run('irc.freenode.net')
+    print __doc__
+
+if __name__ == "__main__":
+    main()
