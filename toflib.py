687086e1c4a3a4fdc94f15ef3c0ab41475507bc9
Christophe-Marie Duquesne <chm.duquesne@gmail.com>
2015-08-30 22:45:01 +0200

    finished pep8ifying toflib

--- toflib.py
+++ toflib.py
@@ -207,29 +207,31 @@ RE_URL = re.compile(
    r"""
     (?xi)
     \b
-    (                           # Capture 1: entire matched URL
+    (                               # Capture 1: entire matched URL
       (?:
         [a-z][\w-]+:                # URL protocol and colon
         (?:
-          /{1,3}                        # 1-3 slashes
-          |                             #   or
-          [a-z0-9%]                     # Single letter or digit or '%'
-                                        # (Trying not to match e.g. "URI::Escape")
+          /{1,3}                    # 1-3 slashes
+          |                         #   or
+          [a-z0-9%]                 # Single letter or digit or '%'
+                                    # (Trying not to match e.g. "URI::Escape")
         )
         |                           #   or
         www\d{0,3}[.]               # "www.", "www1.", "www2." … "www999."
         |                           #   or
-        [a-z0-9.\-]+[.][a-z]{2,4}/  # looks like domain name followed by a slash
+        [a-z0-9.\-]+[.][a-z]{2,4}/  # looks like domain name followed
+                                    # by a slash
       )
       (?:                           # One or more:
-        [^\s()<>]+                      # Run of non-space, non-()<>
-        |                               #   or
+        [^\s()<>]+                  # Run of non-space, non-()<>
+        |                           #   or
         \(([^\s()<>]+|(\([^\s()<>]+\)))*\)  # balanced parens, up to 2 levels
       )+
       (?:                           # End with:
         \(([^\s()<>]+|(\([^\s()<>]+\)))*\)  # balanced parens, up to 2 levels
         |                                   #   or
-        [^\s`!()\[\]{};:'".,<>?«»“”‘’]        # not a space or one of these punct chars
+        [^\s`!()\[\]{};:'".,<>?«»“”‘’]      # not a space or
+                                            # one of these punct chars
       )
     )
     """)
