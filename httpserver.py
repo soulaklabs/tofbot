90c1a317b8cadda656f9415d036bf3ba8c7f443a
Christophe-Marie Duquesne <chmd@chmd.fr>
2015-12-17 11:24:07 +0100

    serving something for openshift to be happy

---
+++ httpserver.py
@@ -0,0 +1,22 @@
+import os
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import SocketServer
+
+class S(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write("<html><body>I am not dead!</body></html>")
+
+    def do_HEAD(self):
+        self._set_headers()
+
+def run(server_class=HTTPServer, handler_class=S):
+    port = int(os.getenv("OPENSHIFT_PYTHON_PORT", "8080"))
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
