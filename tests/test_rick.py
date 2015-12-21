7670746f874fee879067960b3138e8067b3fee45
Etienne Millon <me@emillon.org>
2015-12-21 19:55:09 +0100

    Split test_rick

---
+++ tests/test_rick.py
@@ -0,0 +1,18 @@
+from common import TofbotTestCase
+from httpretty import HTTPretty, httprettified
+
+
+class TestRick(TofbotTestCase):
+
+    @httprettified
+    def test_rick(self):
+        url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        title = 'rickroll'
+        response = '<html><head><title>%s</title></head></html>' % title
+        HTTPretty.register_uri(HTTPretty.GET, url,
+                               body=response,
+                               )
+        self.assertOutput("Keyboard cat v2: %s" % url,
+                          [title,
+                           "We're no strangers to love..."
+                           ])
--- tests/testbot.py
+++ tests/testbot.py
@@ -64,19 +64,6 @@ class TestCase(TofbotTestCase):
         self.bot.send("oho")
         self.assertNoOutput("s/++/a")
 
-    @httprettified
-    def test_rick(self):
-        url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
-        title = 'rickroll'
-        response = '<html><head><title>%s</title></head></html>' % title
-        HTTPretty.register_uri(HTTPretty.GET, url,
-                               body=response,
-                               )
-        self.assertOutput("Keyboard cat v2: %s" % url,
-                          [title,
-                           "We're no strangers to love..."
-                           ])
-
     def test_like(self):
         self.assertOutputLength('!ggg', 0)
         self.bot.send('oh oh', origin='alfred')
