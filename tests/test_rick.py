d89d09759c98d71a7a71bd6edf424740414dd431
Martin Kirchgessner <martin.kirch@gmail.com>
2016-08-18 16:45:59 +0200

    fix test_rick

--- tests/test_rick.py
+++ tests/test_rick.py
@@ -12,7 +12,4 @@ class TestRick(TofbotTestCase):
         HTTPretty.register_uri(HTTPretty.GET, url,
                                body=response,
                                )
-        self.assertOutput("Keyboard cat v2: %s" % url,
-                          [title,
-                           "We're no strangers to love..."
-                           ])
+        self.assertOutput("Keyboard cat v2: %s" % url, "We're no strangers to love...")
