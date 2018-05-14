89af92fa6fdcb8e26b8e07f7278be5a2acc7a7a6
TaTaaaa <daralyfee@hotmail.com>
2018-05-14 17:25:36 +0200

    ajout d'un dictionnaire de bar et de la commande cool !beer

--- plugins/bar.py
+++ plugins/bar.py
@@ -16,71 +16,105 @@ from email.mime.text import MIMEText
 from email.header import Header
 from email.generator import Generator
 from cStringIO import StringIO
-from toflib import Plugin, cmd
+from toflib import Plugin, cmd, InnocentHand
 
 
 class PluginBar(Plugin):
     "A bar necessity plugin"
 
+    def __init__(self, bot):
+        Plugin.__init__(self, bot)
+        # liste des bar autorisés par les Michels
+        liste_bar = {u"L'ouest": [u'ouest',
+                                  u'louest',
+                                  u"l'ouest"],
+                     u"Keep it Weird": [u'keepitweird',
+                                        u'kiw'],
+                     u"Not a Beer": [u'notabeer',
+                                     u"ntb"],
+                     u"Paye ta bière": [
+                         u'payetabiere',
+                         u'ptb',
+                         u'payetabière',
+                         u'paietabière',
+                         u'paietabiere'],
+                     u"Le Bahaus": [u'bahaus',
+                                    u"lebauhaus"],
+                     u"La Bobine": [
+                         u'bob',
+                         u'bobine',
+                         u'labobine',
+                         u'labob'],
+                     u"Le Café du Nord": [
+                         u'lecafedunord',
+                         u'lecafédunord',
+                         u'cafedunord',
+                         u'cafédunord'],
+                     }
+        self.liste_bar = liste_bar
+
+    @cmd(0)
+    def cmd_beer(self, _chan, _args, sender_nick):
+        "envoie un bar au hasard"
+        if sender_nick == "Pebz":
+            self.say(u"random bar! : Dr D")
+        else:
+            random_bar = self.liste_bar.keys()
+            selection = InnocentHand(random_bar)
+            self.say(u"random bar! : " + selection())
+
     @cmd(1, 100)
     def cmd_bar(self, _chan, _args, sender_nick):
-        "writes something"
-
+        "envoie un email aux grelakins"
         heure = _args[0]
-        lieu_s = ' '.join(_args[1:])
-        lieu = lieu_s.lower().strip().replace(" ", "").decode('utf-8')
-
-        # liste des bar autorisés par les Michels
-        liste_bar = [
-            u'ouest',
-            u'keepitweird', u'kiw',
-            u'notabeer',
-            u'paietabiere', u'ptb', u'paietabière',
-            u'bahaus',
-            u'bobine']
+        lieu = ' '.join(_args[1:]).decode('utf-8')
+        lieu = lieu.lower().strip().replace(" ", "")
         if re.match(r"(1[6-9]|2[0-4])+([hH]$|([hH]+" +
                     "[0-5][0-9])|[:]+[0-5][0-9]+)",
                     heure):  # test de la compatibilité du format de l'heure
             # REGLE: on va au bar entre 16h et 24h59, après c'est fermé,
             # avant c'est être alcoolique
-            if lieu in liste_bar:  # teste si le bar proposé est cool
-                from_address = [u"Honorable tofbot",
-                                "tofbot@ouahpiti.info"]
-                pwd = ""
-                recipient = [u"Michels", os.getenv(
-                    "TOFBOT_MAIL", "grelakins@googlegroups.com")]
-                subject = u"Bar ce soir"
-                content = u"""Bonsoir les jeunes,
-Aujourd'hui, certains Michels vont au bar %s a %s.
+            for cle, valeur in self.liste_bar.items():
+                if lieu in valeur:  # teste si le bar proposé est cool
+                    from_address = [u"Honorable tofbot",
+                                    "tofbot@ouahpiti.info"]
+                    pwd = ""
+                    recipient = [u"Michels", os.getenv(
+                        "TOFBOT_MAIL", "grelakins@googlegroups.com")]
+                    subject = u"Bar ce soir"
+                    content = """Bonsoir les jeunes,
+Aujourd'hui, certains Michels vont au bar %s à %s.
 Rejoignez les!
 
 Tofbot, au service de %s
-                """ % (lieu_s, heure, sender_nick)
+                    """ % (cle, heure, sender_nick)
+                    content = content.decode('utf-8')
+                    Charset.add_charset('utf-8',
+                                        Charset.QP, Charset.QP, 'utf-8')
+                    msg = MIMEMultipart('alternative')
+                    msg['Subject'] = "%s" % Header(subject, 'utf-8')
+                    msg['From'] = "\"%s\" <%s>" % (Header(
+                        from_address[0], 'utf-8'), from_address[1])
+                    msg['To'] = "\"%s\" <%s>" % (
+                        Header(recipient[0], 'utf-8'), recipient[1])
 
-                Charset.add_charset('utf-8', Charset.QP, Charset.QP, 'utf-8')
-                msg = MIMEMultipart('alternative')
-                msg['Subject'] = "%s" % Header(subject, 'utf-8')
-                msg['From'] = "\"%s\" <%s>" % (Header(
-                    from_address[0], 'utf-8'), from_address[1])
-                msg['To'] = "\"%s\" <%s>" % (Header(recipient[0],
-                                                    'utf-8'), recipient[1])
+                    txtpart = MIMEText(content, 'plain', 'UTF-8')
+                    msg.attach(txtpart)
 
-                txtpart = MIMEText(content, 'plain', 'UTF-8')
-                msg.attach(txtpart)
-
-                str_out = StringIO()
-                g = Generator(str_out, False)
-                g.flatten(msg)
-                mail_server = "localhost"
-                server = smtplib.SMTP(mail_server,  25)
-                server.ehlo()
-                server.sendmail(from_address[1], recipient[1],
-                                str_out.getvalue())
-                server.quit()
-                # message de confirmation de l'envoi de l'email
-                self.say(u"Michels avertis!")
-            else:  # avertissement bar non autorisé
-                self.say(u"J'envoie pas ce mail, ce bar n'est pas cool!")
+                    str_out = StringIO()
+                    g = Generator(str_out, False)
+                    g.flatten(msg)
+                    mail_server = "localhost"
+                    server = smtplib.SMTP(mail_server,  25)
+                    server.ehlo()
+                    server.sendmail(from_address[1], recipient[1],
+                                    str_out.getvalue())
+                    server.quit()
+                    # message de confirmation de l'envoi de l'email
+                    self.say(u"Michels avertis!")
+                    return
+            # avertissement bar non autorisé
+            self.say(u"J'envoie pas ce mail, ce bar n'est pas cool!")
         else:  # avertissement mauvaise heure
             if re.match(r"^(0[0-9]|1[0-5])", heure):
                 # cas de l'heure trop matinale pour un Michel
