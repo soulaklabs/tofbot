1987754bb7331106692c1d62c6845d0a9469e373
Christophe-Marie Duquesne <chmd@chmd.fr>
2018-06-06 09:37:34 +0200

    Réparation des tests unitaires

--- tests/test_rick.py
+++ tests/test_rick.py
@@ -9,10 +9,6 @@ class TestRick(TofbotTestCase):
         url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
         title = 'rickroll'
         response = '<html><head><title>%s</title></head></html>' % title
-        HTTPretty.register_uri(HTTPretty.GET, url,
-                               body=response,
-                               )
+        HTTPretty.register_uri(HTTPretty.GET, url, body=response)
         self.assertOutput("Keyboard cat v2: %s" % url,
-                          [title,
-                           "We're no strangers to love..."
-                           ])
+                          [ "We're no strangers to love...", title ])
