8c958ad8c7cdf84f56899204d7062eb2aec073d3
Martin Kirchgessner <martin.kirch@gmail.com>
2025-03-09 14:52:40 +0100

    making our planet great again by not computing distance when its lower bound is too high

--- src/tofbot/plugins/dassin.py
+++ src/tofbot/plugins/dassin.py
@@ -25,20 +25,21 @@ class PluginDassin(Plugin):
 
     async def handle_msg(self, msg_text, chan, nick):
         searched = msg_text.lower()
-        minDist = 9999999
+        max_dist = len(searched) / 3
+        min_dist = 9999999
         best = ""
 
         for song in self.songs:
             try:
-                i = 0
-                for line in song:
+                for i, line in enumerate(song):
+                    if abs(len(line) - len(searched)) >= max_dist:
+                        continue
                     dist = distance(line, searched)
-                    if dist < minDist:
+                    if dist < min_dist:
                         best = song[i + 1]
-                        minDist = dist
-                    i += 1
+                        min_dist = dist
             except IndexError:
                 pass
 
-        if len(best) > 3 and minDist < (len(searched) / 3):
+        if len(best) > 3 and min_dist < max_dist:
             await self.say(best)
